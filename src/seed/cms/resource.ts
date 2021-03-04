ce20d93eb2b49e0858f5f1c9790e5e89d891a493
kain <zhangtqx@vip.qq.com>
2021-03-04 09:02:53 +0800

    update

--- src/seed/cms/resource.ts
+++ src/seed/cms/resource.ts
@@ -4,13 +4,24 @@ import { Resource } from '@entity/framework/resource';
 export async function resource(entityManager: EntityManager, vars: any) {
   await entityManager.getRepository(Resource).insert([
     {
-      key: 'media',
+      key: 'cms',
       parent: 'origin',
+      name: { zh_cn: '内容管理', en_us: 'CMS' },
+      nav: 1,
+      router: 0,
+      policy: 0,
+      icon: 'table',
+      create_time: vars.time,
+      update_time: vars.time,
+    },
+    {
+      key: 'media',
+      parent: 'cms',
       name: { zh_cn: '多媒体管理', en_us: 'Media' },
       nav: 1,
       router: 0,
       policy: 0,
-      icon: 'play-circle',
+      icon: null,
       create_time: vars.time,
       update_time: vars.time,
     },
