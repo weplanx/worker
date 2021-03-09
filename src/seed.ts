68b94523c542b40a8cf396dd5dc52fc08ae70996
kain <zhangtqx@vip.qq.com>
2021-03-09 23:48:09 +0800

    update

--- src/seed.ts
+++ src/seed.ts
@@ -1,8 +1,25 @@
 import { createConnection } from 'typeorm';
+import { acl } from './seed/acl';
+import { resource } from './seed/resource';
+import { policy } from './seed/policy';
+import { permission } from './seed/permission';
+import { role } from './seed/role';
+import { admin } from './seed/admin';
 
 createConnection().then(async connection => {
-  const vars = {
-    time: Math.floor(new Date().getTime() / 1000),
-  };
+  await connection.transaction(async entityManager => {
+    const args = [
+      entityManager,
+      {
+        time: Math.floor(new Date().getTime() / 1000),
+      },
+    ];
+    const tasks = [
+      acl, resource, policy, permission, role, admin,
+    ];
+    for (const task of tasks) {
+      await Reflect.apply(task, undefined, args);
+    }
+  });
   await connection.close();
 });
