228e12d5617c915fdee26bed15e5f719d20fbd67
kain <zhangtqx@vip.qq.com>
2021-03-28 17:23:12 +0800

    update

---
+++ src/entity/product.ts
@@ -0,0 +1,20 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Id, Status, Timestamp } from '@type';
+
+@Entity()
+export class Shop {
+  @PrimaryGeneratedColumn(Id)
+  id: number;
+
+  @Column({ type: 'varchar', comment: '产品名称' })
+  name: string;
+
+  @Column(Status())
+  status: number;
+
+  @Column(Timestamp)
+  create_time: number;
+
+  @Column(Timestamp)
+  update_time: number;
+}
